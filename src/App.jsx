51cf2ddc92ccd84419cccbde6296168bfb0b5332
Isabel lopez <123342471+isabel2306@users.noreply.github.com>
2023-04-25 23:33:58 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { Productlist } from './components/Productlist';
+
+function App() {
+	const [allProducts, setAllProducts] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [countProducts, setCountProducts] = useState(0);
+
+	return (
+		<>
+			<Header
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+			<Productlist
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+		</>
+	);
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        id: 1,
+        img: "https://silviacaeirocakes.com.ar/wp-content/uploads/2021/01/Gelatina-decorada-3D.jpg",
+        nameprodut: "Gelatinas",
+        price: 70,
+        quantity: 1,
+
+    },
+    {
+        id: 1,
+        img: 'https://static.onecms.io/wp-content/uploads/sites/21/2017/02/24/3a23ae4b-48b7-44eb-96a7-0e8e755683b6-2000.jpg',
+        nameprodut: "Pastel chocolate",
+        price: 200,
+        quantity: 1,
+
+    },
+    {
+        id: 1,
+        img: 'https://img-global.cpcdn.com/recipes/6592d59806f854ae/400x400cq70/photo.jpg',
+        nameprodut: "Galletas de nuez",
+        price: 50,
+        quantity: 1,
+
+    },
+    {
+        id: 1,
+        img: 'https://www.recetasnestle.com.mx/sites/default/files/srh_recipes/54c6cbcbc6d611e056122d64560cd9c1.jpg',
+        nameprodut: "Flan napolitano",
+        price: 90,
+        quantity: 1,
+
+    },
+    {
+        id: 1,
+        img: 'https://www.recetas360.com/wp-content/uploads/2023/03/receta-de-muffins-de-chocolate.jpeg',
+        nameprodut: "Muffin de chocolate",
+        price: 30,
+        quantity: 1,
+
+    },
+]
